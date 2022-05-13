8982acecfb198ceb2bb8ad873b9e80db408a9e60
Pavel Solovev <p.solovev@articul.ru>
2022-05-13 11:06:45 +0400

    add Reporter

--- src/Summary.ts
+++ src/Summary.ts
@@ -4,10 +4,15 @@ export interface Analyzer {
   run(matches: MatchData[]): string;
 }
 
-export interface OutTarget {
+export interface OutputTarget {
   print(report: string): void;
 }
 
 export class Summary {
-  constructor(public analyzer: Analyzer, public outputTarget: OutTarget) {}
+  constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
+
+  buildAndPrintReport(matches: MatchData[]): void {
+    const output = this.analyzer.run(matches);
+    this.outputTarget.print(output);
+  }
 }
